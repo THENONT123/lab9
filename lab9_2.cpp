82029377fb7b30d5f048c6d3c9e9e446608c3e44
THENONT123_ <Nonthaphatc@hotmail.com_>
2024-12-17 16:37:48 +0700

    finish

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -2,6 +2,17 @@
 using namespace std;
 
 //Write the function printO() here
+int printO(int x,int y){
+	if(x<=0 or y<=0){
+		cout<<"Invalid input";
+	}
+	for(int i=0;i<y;i++){
+		for (int j = 0;j<x;j++){
+			cout<<'O';
+		}
+		cout<<"\n";
+	}
+}
 
 int main(){
 	
