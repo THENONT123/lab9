228b1632e5a0753de7dd161e0541ba8a53e22c46
THENONT123_ <Nonthaphatc@hotmail.com_>
2024-12-17 15:48:35 +0700

    finissh

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -3,9 +3,13 @@
 using namespace std;
 
 int main(){	
+	double prev,ini,total,pay,dog,interest;
 	cout << "Enter initial loan: ";
+	cin>>ini;
 	cout << "Enter interest rate per year (%): ";
+	cin>>interest;
 	cout << "Enter amount you can pay per year: ";
+	cin>>pay;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -21,13 +25,24 @@ int main(){
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
 	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
-	
+	int i=1;
+	while(ini>0){
+		prev = ini;
+		dog = prev*(interest/100.0);
+		total = prev+dog;
+		ini = total-pay;
+		if(ini<0){
+			pay += ini;
+			ini = 0;
+		}
+		cout << setw(13) << left << i; 
+		cout << setw(13) << left << prev; 
+		cout << setw(13) << left << dog; 
+		cout << setw(13) << left << total;
+		cout << setw(13) << left << pay;
+		cout << setw(13) << left << ini;
+		cout<<"\n";
+		i++;
+	}
 	return 0;
 }
